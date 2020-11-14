440690ab2a32f09a883de0513c3276eb421c3b3e
Adam Jenča <jenca.adam@gmail.com>
2020-11-14 20:06:23 +0100

    v2.0.; binary files encoding and program is really fast.

--- fcad.py
+++ fcad.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 
 import os,sys,random,getpass,hashlib,subprocess,argparse,zipfile
-maxchr=9999
-chrvals=[]
-for i in range(maxchr):
-    if chr(i)!='':
-        chrvals.append(chr(i))
-
+maxchr=256
+chrvals=range(maxchr)
 class FCaDError(Exception):pass
 class Hasher:
 
-    def __init__(self, strenght):
-        self.strenght=strenght
-        if self.strenght>100:
-            raise FCaDError('Strenght must be<100')
+    def __init__(self):
         self._hashdict={}
         self.pw=hashlib.sha224(bytes(getpass.getpass('Enter new password for this file:'),encoding='utf-8')).hexdigest()
     def __getitem__(self,key):
@@ -25,28 +18,19 @@ class Hasher:
         raise FCaDError( "Hasher is read-only dict-like object, not dict")
     def update(self):
         print("Generating hashdict...WARNING: This might take a few seconds.")
-        self._hashdict=dict(zip((chr(i)for i in range(maxchr)),(self._makehashdict(self.strenght))))       
+        shuffle=list(range(maxchr))
+        random.shuffle(shuffle)
+        print(shuffle)
+        self._hashdict=dict(zip(range(maxchr),(shuffle)))       
         print("Sucesfully updated.")
-    def _makehashdict(self, stren):
-        startstrs=[]
-        strng=""
-        for e in range(maxchr):
-            strng=""
-            for a in range(stren):
-            
-                strng+=random.choice(chrvals)
-            startstrs.append(strng)
-        return(tuple(startstrs))
-    
+        print(self._hashdict)
+         
     def _makelist(self):
         lst=[]
         lst.append(str(self.pw))
         lst.append('\n')
-        lst.append(str(self.strenght))
-        lst.append('\n')
         for i in self._hashdict.values():
-            for a in i:
-                lst.append(str(ord(a)**45)+'\n')
+            lst.append(str(i**45)+'\n')
             lst.append('\n')
         return (tuple(lst))
 
@@ -57,28 +41,19 @@ class Hasher:
         self.file_to_code=file_to_code
         try:
 
-             self.file=open(self.file_to_code)
+             self.file=open(self.file_to_code,'rb')
         except FileNotFoundError:
-
-            self.g=open(self.file_to_code,'w') 
-            sys.stdout.write("WARNING: No such file or directory:{}!\n".format(self.file_to_code))
-
-            sys.stdout.write("WARNING: File with the entered name will be created and used for coding!\n")
-            print("Enter text to be coded:")
-            self.g.write(sys.stdin.readline())
-            self.g.close()
-            self.file=open(self.file_to_code)
-
-        def encode(string,dictionary):
+            raise FCaDError(f'No such file or directory:{self.file_to_code}')
+        def encode(lst,dictionary):
             z=[]
-            for i in string:
+            for i in lst:
                 z.append(dictionary[i])
-            return ''.join(z)
+            return z
         self.zoz1=[]
 
-        self.coded=encode(self.file.read(),self._hashdict)
-        self.file=open(os.path.splitext(self.file_to_code)[0],'w')
-        self.file.write(self.coded)
+        self.coded=encode(list(self.file.read()),self._hashdict)
+        self.file=open(os.path.splitext(self.file_to_code)[0],'wb')
+        self.file.write(bytes(self.coded))
         self.file.close()
         self.keyfilename=os.path.splitext(self.file_to_code)[0]+".fcadk"
         self.keyfile=open(self.keyfilename,'w')
@@ -101,56 +76,53 @@ class Decoder():
         self.paswd=hashlib.sha224(bytes(getpass.getpass('Enter password:'),encoding='utf-8')).hexdigest()
         if os.path.splitext(self.filename)[1]!=".fcad":
             raise FCaDError("File must be fcad file not{}".format(os.path.splitext(self.filename)[1]))
-        subprocess.call(['unzip',filename])
+        subprocess.call(['unzip',filename,'-o'])
         self.filename1=os.path.splitext(filename)[0]+'.fcadk'
         self.file=open(self.filename1)
         self.filename2=os.path.splitext(filename)[0]
-        self.file2=open(self.filename2)
+        self.file2=open(self.filename2,'rb')
         
     
     def decodekeyfile(self):
         '''This decodes current keyfile.'''
         def decode(integer):
-            try:
-                return (chr(round(integer**(1/45))))
-            except ValueError:
-                return("")
+            return (round(integer**(1/45)))
         self.paswd1=self.file.readline().strip()
         if self.paswd!=self.paswd1:
             print('This password is not correct.')
             self.do=False
             return
-        self.strength=int(self.file.readline().strip())
         self._hdict={}
         self.lineindex=0
-        chars=[]
+        byte=b''
         for line in self.file:
             
             if not line:
                break
             if line != '\n':
-                chars.append(decode(int(line)))
+                byte=decode(int(line))
             else:
-                self._hdict["".join(chars)]=chr(self.lineindex)
-
+                self._hdict[byte]=self.lineindex
+        
                 self.lineindex+=1
                 chars=[]
-        self.file.close
+        self.file.close()
+        print(*self._hdict)
         
     def decodefileby(self):
         '''This decodes current "file_to_decode" according to decoded keyfile'''
         if self.do:
             self.zoz1=[]
             while True:
-                e=self.file2.read(self.strength)
+                e=self.file2.read(1)
                 if not e:
                     break
                 try:
-                    self.zoz1.append(self._hdict[e])
+                    self.zoz1.append(self._hdict[list(e)[0]])
                 except KeyError:
-                    raise FCaDError('Key does not exist. Are you sure the file is not decoded?')
-            self.decodedstr=''.join(self.zoz1)
-            self.file2=open(self.filename2,'w')
+                    raise FCaDError(f'{e}:Key does not exist. Are you sure the file is not decoded?')
+            self.decodedstr=bytes(self.zoz1)
+            self.file2=open(self.filename2,'wb')
             self.file2.write(self.decodedstr)
             self.file2.close()
             print(self.decodedstr)
@@ -187,7 +159,7 @@ class PasswordGenerator:
             self.password.append(random.choice(random.choice(self.includes)))
        
         return(''.join(self.password))
-acceptable_commands={'codefile':{'name':'codefile','args':2,'command':lambda strenght,filename:exec('h=Hasher(int(strenght))\nh.update()\nh.codefile(filename)\n'),'description':'codefile:args:strenght,filename.Codes the file by actual Hasher.'},'decodefile':{'name':'decodefile','args':1,'command':lambda filename1:exec('d=Decoder(filename1)\nd.decodekeyfile()\nd.decodefileby()'),'description':'decodefile:args:filename1,filename2.Decodes file according to filenmae2 according to file according to filename2'},'generpw':{'name':'generpw','args':5,'command':lambda l,u,s,a,st:exec('p=PasswordGenerator(bool(int(l)),bool(int(u)),bool(int(s)),bool(int(a)),int(st))\nprint(next(iter(p)))'),'description':'generpw:args:include_lowercase,include_uppercase,include_symbols,include_another,pronounciablelenght.Generates password.'},'exit':{'name':'exit','args':0,'command':sys.exit,'description':'exit:args:none.Exits prompt.'},'help':{'name':'help','args':0,'command':lambda:print(*[i['description']for i in acceptable_commands.values()],sep='\n'),'description':'help:args:none.Prints help string'}}    
+acceptable_commands={'codefile':{'name':'codefile','args':1,'command':lambda filename:exec('h=Hasher()\nh.update()\nh.codefile(filename)\n'),'description':'codefile:args:strenght,filename.Codes the file by actual Hasher.'},'decodefile':{'name':'decodefile','args':1,'command':lambda filename1:exec('d=Decoder(filename1)\nd.decodekeyfile()\nd.decodefileby()'),'description':'decodefile:args:filename1,filename2.Decodes file according to filenmae2 according to file according to filename2'},'generpw':{'name':'generpw','args':5,'command':lambda l,u,s,a,st:exec('p=PasswordGenerator(bool(int(l)),bool(int(u)),bool(int(s)),bool(int(a)),int(st))\nprint(next(iter(p)))'),'description':'generpw:args:include_lowercase,include_uppercase,include_symbols,include_another,pronounciablelenght.Generates password.'},'exit':{'name':'exit','args':0,'command':sys.exit,'description':'exit:args:none.Exits prompt.'},'help':{'name':'help','args':0,'command':lambda:print(*[i['description']for i in acceptable_commands.values()],sep='\n'),'description':'help:args:none.Prints help string'}}    
 def main():
     try:
         a=input('>')
@@ -212,8 +184,8 @@ def main():
         main()
     except EOFError:
         exit()
-    except Exception as error:
-        parse_error(error)
+    #except Exception as error:
+    #    parse_error(error)
 def randchar():
     return(random.choice(chrvals))
 def randbyte():
@@ -234,7 +206,6 @@ parser=argparse.ArgumentParser(description='Encodes and decodes files')
 sparsers=parser.add_subparsers(help='blah help')
 
 codefile_prsr=sparsers.add_parser('codefile',help='sjhjshjshjshhjshj')
-codefile_prsr.add_argument('strenght',type=int,help='iewik')
 codefile_prsr.add_argument('file',type=str,help='gah')
 
 decodefile=sparsers.add_parser('decodefile',help='hsahsa')
@@ -253,7 +224,7 @@ if __name__=='__main__':
         try:
             if 'codefile' in sys.argv:
                 codefile_args= parser.parse_args(sys.argv[1:])
-                h=Hasher(codefile_args.strenght)
+                h=Hasher()
                 h.update()
                 h.codefile(codefile_args.file)
             elif 'decodefile' in sys.argv:
@@ -269,8 +240,8 @@ if __name__=='__main__':
             sys.exit()
         except EOFError:
             sys.exit()
-        except Exception as error:
-            parse_error(error)
+       # except Exception as error:
+       #     parse_error(error)
         
 
 
